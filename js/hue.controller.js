56eff2d1c7eb26d753f6ef40007f4d2d074be03c
Christoph Ketzler <christoph@ketzler.de>
2016-10-26 16:03:07 +0200

    First working brightness version

--- js/hue.controller.js
+++ js/hue.controller.js
@@ -1,47 +1,79 @@
 angular.module('gui.hue', []).controller('HueCtrl', function HueCtrl($scope, $interval, $timeout,$rootScope,$http) {
-
-	//$scope.url = "http://raspberrypi/netatmo/get.php";
+	
+	$scope.ip = '192.168.178.54';
+	$scope.key = '87wTw9H651KevYIWhQl2cw7K8KioqN4eLGe0N1Fv';
+	$scope.url = 'http://'+$scope.ip+'/api/'+$scope.key+'/';
+	$scope.lights = [];
+	
+	$scope.briSliderOptions = {
+		    floor: 0,
+		    ceil: 254,
+		    vertical: true
+		};
 
 	$scope.init = function() {
 		console.log('start HueCtrl');
+		$scope.updateLights();
+		
+
 	};
+	
+	$scope.updateLights = function()
+	{
+		$http({
+			method : 'GET',
+			url : $scope.url+'lights'
+		}).then(function successCallback(response) {
+			console.log(response.data);
+			// for (var i=0; i < response.data.length; i++) {
+			for (var lightNr in response.data)
+			{
+				var t = response.data[lightNr];
+				t.options = angular.copy($scope.briSliderOptions);
+				t.options.id = $scope.lights.length;
+				t.options.onChange = $scope.handleChange;
+			  $scope.lights.push(t);
+			};
+			console.log($scope.lights);
+		}, function errorCallback(response) {
+			console.log(response.status);
+			
+		});
+	};
+	
+	$scope.handleChange = function(e)
+	{
+		console.log($scope.lights[e]);
+		
+		$http({
+			method : 'PUT',
+			url : $scope.url+'lights/'+(e+1)+'/state',
+			data : {"on": true, "bri":$scope.lights[e].state.bri}
+		}).then(function successCallback(response) {
+			console.log(response.data);
+			
+		}, function errorCallback(response) {
+			console.log(response.status);
+			
+		});
+		
+	};
+	
+	$scope.$watch('brightness',function(oldVal,newVal)
+	{
+		console.log('brightness', $scope.brightness);
+	});
 	$scope.init();
-	$scope.values = {};
-	$scope.test = "hue";
-	$scope.counter = 1;
 	$scope.brightness = 100;
 	$scope.visible=true;
+	
+	
+	
+	//$scope.update = 
+	
 	$scope.refresh = function()
 
 	{
-		$scope.counter++;
-		// $http({
-			// method : 'GET',
-			// url : $scope.url
-		// }).then(function successCallback(response) {
-			// // this callback will be called asynchronously
-			// // when the response is available
-			// console.log(response.data);
-			// $scope.values.inside = {};
-			// $scope.values.inside.temp = response.data.inside.Temperature;
-			// $scope.values.inside.temp_max = response.data.inside.max_temp;
-			// $scope.values.inside.temp_min = response.data.inside.min_temp;
-			// $scope.values.inside.pressure = Math.round(response.data.inside.Pressure);
-			// $scope.values.inside.co2 = Math.round(response.data.inside.CO2);
-			// $scope.values.inside.humidity = Math.round(response.data.inside.Humidity);
-// 			
-			// $scope.values.outside = {};
-			// $scope.values.outside.temp = response.data.outside.Temperature;
-			// $scope.values.outside.temp_max = response.data.outside.max_temp;
-			// $scope.values.outside.temp_min = response.data.outside.min_temp;
-			// $scope.values.outside.humidity = Math.round(response.data.outside.Humidity);
-			// // $scope.picUrl = response.data;
-		// }, function errorCallback(response) {
-			// // called asynchronously if an error occurs
-			// // or server returns response with an error status.
-			// console.log(response.status);
-// 			
-		// });
 	};
 	$scope.refresh();
 
